59f41d00dc2b2808fed7021238da1ca7719851a3
Paweł <paweltar@gmail.com>
2016-03-18 16:10:15 +0100

    Add imagesLoaded, images

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-$( document ).ready(function() {
-  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+$(document).ready(function() {
   particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
+  });
+
+  var $grid = $('.gallery-wrapper').masonry({
+    itemSelector: '.gallery-card',
+    fitWidth: true,
+    gutter: 10
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress(function() {
+    $grid.masonry('layout');
   });
 });
