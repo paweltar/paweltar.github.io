161d546fe758233f25ce1c2de90f10e9da99dc9b
Paweł <paweltar@gmail.com>
2016-03-23 14:50:30 +0100

    Add dropcap to first paragraph

--- js/default-index.js
+++ js/default-index.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  console.log('js initialized');
+  // We retrieve our drop cap elements using a class selector...
+    var dropcaps = document.querySelectorAll(".dropcap");
+    // ...then give them a height of three lines.
+    // By default, the drop cap's baseline will also be the third paragraph line.
+    window.Dropcap.layout(dropcaps, 2, 2);
 });
